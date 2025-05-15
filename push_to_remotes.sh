68cec58c2c148d904c9c4645a495b1f24c703a6e
shannon156 <jianghao_85@qq.com>
2025-05-15 13:27:39 +0800

    2025 05 15

---
+++ push_to_remotes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 获取当前日期，格式为 "YYYY MM DD"
+DATE=$(date "+%Y %m %d")
+
+# 添加所有更改
+git add .
+
+# 提交更改，附带日期作为 commit message
+git commit -m "$DATE"
+
+# 推送到两个远程仓库：origin 和 github
+git push origin master
+git push github master
+
